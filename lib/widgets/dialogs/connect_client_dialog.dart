57b2773f81fa478342a1a003914500d667dbe407
ushieru <uzielcocolan@gmail.com>
2023-10-04 18:12:21 -0600

    Feat: add  QR connection

--- lib/widgets/dialogs/connect_client_dialog.dart
+++ lib/widgets/dialogs/connect_client_dialog.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:qr_flutter/qr_flutter.dart';
+import 'package:total_pos/models/settings.dart';
 import 'package:total_pos/services/http_services/utils_service.dart';
 
 class _ConnectClientDialog extends StatefulWidget {
@@ -10,11 +12,11 @@ class _ConnectClientDialog extends StatefulWidget {
 
 class _ConnectClientDialogState extends State<_ConnectClientDialog> {
   final _utilsServices = UtilsService();
-  String? services;
+  String? serverUrl;
 
   @override
   void initState() {
-    _utilsServices.getInfo().then((info) => setState(() => services = info));
+    _utilsServices.getInfo().then((info) => setState(() => serverUrl = info));
     super.initState();
   }
 
@@ -23,7 +25,19 @@ class _ConnectClientDialogState extends State<_ConnectClientDialog> {
           title: const Text('Conectar Cliente'),
           contentPadding: const EdgeInsets.all(30),
           children: [
-            Text(services ?? 'Cargando...'),
+            if (serverUrl != null)
+              SizedBox(
+                  width: 200,
+                  child: QrImageView(
+                    data: '@connect:$serverUrl:8080',
+                    dataModuleStyle: QrDataModuleStyle(
+                        color: Settings.primaryColor.shade800,
+                        dataModuleShape: QrDataModuleShape.square),
+                    eyeStyle: QrEyeStyle(
+                        color: Settings.primaryColor.shade800,
+                        eyeShape: QrEyeShape.square),
+                  )),
+            Text('$serverUrl:8080'),
           ]);
 }
 
