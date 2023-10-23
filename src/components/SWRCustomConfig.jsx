d6f785101478ba5ada6e4209d28c967769483486
ushieru <uzielcocolan@gmail.com>
2023-10-23 00:31:55 -0600

    Feat: impl useConfigStore

--- src/components/SWRCustomConfig.jsx
+++ src/components/SWRCustomConfig.jsx
@@ -1,13 +1,15 @@
 
 import { SWRConfig } from 'swr'
 import { useSessionStore } from '@/stores/useSessionStore'
+import { useConfigStore } from '@/stores/useConfigStore'
 
 export const SWRCustomConfig = ({ children }) => {
     const session = useSessionStore(s => s.session)
+    const urlServer = useConfigStore(s => s.urlServer)
 
     return <SWRConfig value={{
         refreshInterval: 3000,
-        fetcher: (url) => fetch(`http://localhost:8080${url}`, { headers: { 'Authorization': `Bearer ${session.token}` } }).then(res => res.json())
+        fetcher: (url) => fetch(`${urlServer}${url}`, { headers: { 'Authorization': `Bearer ${session.token}` } }).then(res => res.json())
     }}>
         {children}
     </SWRConfig>
