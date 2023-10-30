d45f91c41737fd60a5a0c6409ed3880314775224
ushieru <uzielcocolan@gmail.com>
2023-10-30 12:44:03 -0600

    Feat: close waiter session

--- src/routes/waiter/WaiterDashboardRoute.jsx
+++ src/routes/waiter/WaiterDashboardRoute.jsx
@@ -1,4 +1,23 @@
+import {
+  Card,
+  CardHeader,
+  Button,
+} from "@nextui-org/react";
+import { MdExitToApp } from 'react-icons/md'
+import { useSessionStore } from "@/stores/useSessionStore"
+
 export const WaiterDashboardRoute = () => {
-  return <div className="h-screen w-screen">
+  const closeSession = useSessionStore(s => s.closeSession)
+
+  return <div className="h-screen w-screen p-5">
+    <Card className="mb-5">
+      <CardHeader className="justify-between">
+        Mesero
+        <Button onPress={closeSession}>
+          Cerrar Sesion
+          <MdExitToApp className="text-lg" />
+        </Button>
+      </CardHeader>
+    </Card>
   </div>
 }
