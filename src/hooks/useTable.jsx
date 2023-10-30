c266edff736efcd76c39b9f69c20b91736e72af7
ushieru <uzielcocolan@gmail.com>
2023-10-30 14:00:30 -0600

    Feat: CRUD tables

---
+++ src/hooks/useTable.jsx
@@ -0,0 +1,61 @@
+import { useConfigStore } from "../stores/useConfigStore"
+import { useSessionStore } from "../stores/useSessionStore"
+
+export const useTable = () => {
+    const urlServer = useConfigStore(state => state.urlServer)
+    const session = useSessionStore(state => state.session)
+    const url = `${urlServer}/tables`
+    return {
+        /**
+         * Create a new table
+         * @param {string} name 
+         * @returns {Promise<object>}
+         */
+        createTable: async (name) => {
+            const dto = { name }
+            const init = {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Authorization': `Bearer ${session.token}`
+                },
+                body: JSON.stringify(dto)
+            }
+            return fetch(url, init)
+                .then(r => r.json())
+        },
+        /**
+         * Delete a table
+         * @param {number} id 
+         * @param {{name: string, pos_x: number, pos_y: number}} table
+         * @returns {Promise<object>}
+         */
+        updateTable: async (id, table) => {
+            const init = {
+                method: 'PUT',
+                headers: {
+                    'Authorization': `Bearer ${session.token}`,
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(table)
+            }
+            return fetch(`${url}/${id}`, init)
+                .then(r => r.json())
+        },
+        /**
+         * Delete a table
+         * @param {number} id 
+         * @returns {Promise<object>}
+         */
+        deleteTable: async (id) => {
+            const init = {
+                method: 'DELETE',
+                headers: {
+                    'Authorization': `Bearer ${session.token}`
+                }
+            }
+            return fetch(`${url}/${id}`, init)
+                .then(r => r.json())
+        },
+    }
+}
--- src/routes/admin/AdminTablesRoute.jsx
+++ src/routes/admin/AdminTablesRoute.jsx
@@ -2,12 +2,16 @@ import { useEffect, useState } from "react"
 import {
     Card,
     CardHeader,
-    CardBody
+    CardBody,
+    Input,
+    Button,
 } from "@nextui-org/react"
 import useSWR from "swr"
+import { useTable } from "@/hooks/useTable"
 
 export const AdminTablesRoute = () => {
-    const { data } = useSWR('/tables')
+    const { data, mutate } = useSWR('/tables')
+    const { createTable, updateTable, deleteTable } = useTable()
     const [tables, setTables] = useState([])
 
     useEffect(() => {
@@ -28,20 +32,77 @@ export const AdminTablesRoute = () => {
         setTables(tablesOrdered)
     }
 
+    const onSubmit = (event) => {
+        event.preventDefault()
+        const name = event.target.name.value
+        createTable(name).then(r => {
+            if (r.code) return
+            event.target.name.value = ''
+            mutate()
+        })
+    }
+
+    const onDrag = (table) => (e) => {
+        e.dataTransfer.setData("table", JSON.stringify(table))
+    }
+
+    const onDrop = (x, y) => (e) => {
+        e.preventDefault()
+        const table = JSON.parse(e.dataTransfer.getData("table"))
+        updateTable(table.id, { name: table.name, pos_x: x, pos_y: y })
+            .then(_ => mutate())
+    }
+
+    const onDropDelete = (e) => {
+        e.preventDefault()
+        const table = JSON.parse(e.dataTransfer.getData("table"))
+        deleteTable(table.id).then(_ => mutate())
+    }
+
     return <div>
         <Card className="mb-5">
             <CardHeader className="text-xl font-semibold">Mesas</CardHeader>
+        </Card>
+        <Card className="mb-5">
+            <CardHeader className="justify-between">
+                <form className="flex gap-2" onSubmit={onSubmit}>
+                    <Input name="name" />
+                    <Button type="submit">Nueva mesa</Button>
+                </form>
+                <div
+                    className="border border-dashed bg-red-500 px-2 py-1"
+                    onDrop={onDropDelete}
+                    onDragOver={(e) => e.preventDefault()}
+                >
+                    Suelte aqui para eliminar
+                </div>
+            </CardHeader>
         </Card>
         <Card>
             <CardBody className="grid gap-1 grid-cols-10">
                 {
                     tables.map((table, i) => {
+                        const x = i % 10
+                        const y = Math.floor(i / 10)
                         if (!table)
-                            return <div key={i} className="aspect-square bg-default-500"></div>
-                        return <div key={i} className="aspect-square bg-blue-500 grid place-items-center">{table.name}</div>
+                            return <div
+                                key={i}
+                                onDrop={onDrop(x + 1, y + 1)}
+                                onDragOver={(e) => e.preventDefault()}
+                                className="aspect-square bg-default-500"
+                            />
+                        return <div
+                            key={i}
+                            draggable={true}
+                            onDragStart={onDrag(table)}
+                            className="aspect-square bg-blue-500 grid place-items-center"
+                        >
+                            {table.name}
+                        </div>
                     })
                 }
             </CardBody>
         </Card>
     </div>
 }
+
