a0c7828a567b241eee0fe12a273b0f45f339ebd1
ushieru <uzielcocolan@gmail.com>
2023-11-01 10:21:54 -0600

    Feat: add tables

--- src/routes/cashier/CashierDashboardRoute.jsx
+++ src/routes/cashier/CashierDashboardRoute.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import useSWR from "swr"
 import {
   Card,
@@ -10,6 +11,8 @@ import { useNavigate } from "react-router-dom";
 import { useSessionStore } from "@/stores/useSessionStore";
 import { useTicket } from "@/hooks/useTicket";
 import { FilterBuilder } from "@/utils/filterBuilder";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+import { orderTables } from "@/utils/tableUtils";
 
 export const CashierDashboardRoute = () => {
   const navigate = useNavigate();
@@ -20,9 +23,19 @@ export const CashierDashboardRoute = () => {
     .addFilter('ticket_status', '=', '"open"')
     .build()
   const { data: openTickets } = useSWR(`/tickets?${filters}`)
+  const { data: rawTables } = useSWR('/tables')
+  const [tables, setTables] = useState([])
+  const isMobile = useMediaQuery("only screen and (max-width : 640px)")
   const { createTicket } = useTicket()
 
-  return <div className="h-screen w-screen p-5">
+  useEffect(() => {
+    if (rawTables) {
+      const t = orderTables(rawTables, isMobile)
+      setTables(t)
+    }
+  }, [rawTables, isMobile])
+
+  return <div className="h-screen w-screen p-5 overflow-auto">
     <Card className="mb-5">
       <CardHeader className="justify-between">
         Cajero
@@ -40,7 +53,7 @@ export const CashierDashboardRoute = () => {
         </Button>
       </CardHeader>
     </Card>
-    <Card>
+    <Card className="mb-5">
       <CardHeader>Tickets Abiertos</CardHeader>
       <CardBody className="grid grid-cols-5 gap-3">
         {
@@ -52,6 +65,32 @@ export const CashierDashboardRoute = () => {
           >
             <CardHeader>Ticket #{t.id}</CardHeader>
           </Card>)
+        }
+      </CardBody>
+    </Card>
+    <Card>
+      <CardBody className={`grid gap-1 ${isMobile ? "grid-cols-5" : "grid-cols-10"}`}>
+        {
+          tables.map((table, i) => {
+            if (!table)
+              return <div
+                key={i}
+                className="aspect-square bg-default-500"
+              />
+            if (!table.ticket_id)
+              return <div
+                key={i}
+                className="aspect-square rounded-medium bg-blue-500 grid place-items-center"
+              >{table.name}</div>
+            return <Button
+              key={i}
+              onPress={() => navigate('/tickets/' + table.id)}
+              color="secondary"
+              className="h-full min-w-0"
+            >
+              {table.name}
+            </Button>
+          })
         }
       </CardBody>
     </Card>
