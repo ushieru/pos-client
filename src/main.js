448f7ea2a9b395fe9c0e13e5016dc95f869782a5
ushieru <uzielcocolan@gmail.com>
2024-06-06 11:27:27 -0600

    Feat: extend dayjs

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import { VueQueryPlugin } from '@tanstack/vue-query'
-import 'material-symbols';
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/es-mx'
+import 'material-symbols'
 import './style.css'
 import { router } from './router'
 import App from './App.vue'
@@ -8,6 +11,8 @@ import { PosSingleton } from './services/pos-service'
 
 const host = localStorage.getItem('x-pos-host') ?? `http://${window.location.hostname}:8080`
 new PosSingleton(host)
+dayjs.locale('es-mx')
+dayjs.extend(relativeTime)
 
 createApp(App)
     .use(VueQueryPlugin)
